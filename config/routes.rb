f79322f3eb3979bb91e8bd9f2b7a746e08716445
mardiniii <sezama03@hotmail.com>
2016-11-12 11:29:03 -0500

    Define routes for competitors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'competitors/index'
 
   root 'pages#game'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :competitors, only: [ :create, :index ]
 end
