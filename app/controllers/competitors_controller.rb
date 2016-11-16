0c08aeac3b778cc168c070c5ad409d13dec05734
mardiniii <sezama03@hotmail.com>
2016-11-15 19:20:07 -0500

    Add logic to process competitors score update action

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,4 +1,26 @@
 class CompetitorsController < ApplicationController
-  def index
+
+  def create
+    email = params[:competitor][:email]
+    score = params[:competitor][:score].to_i
+    competitor = Competitor.find_by_email(email)
+
+    if competitor
+      competitor.update(competitor_params) if competitor.score < score
+    else
+      Competitor.create(competitor_params)
+    end
+
+    redirect_to competitors_path
   end
+
+  def index
+    @competitors = Competitor.all.order(score: :desc).limit(10)
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def competitor_params
+      params.require(:competitor).permit(:email, :score)
+    end
 end
